53e762796e3a44995cdd4c9e3791b5fa1381df1e
Huafeng Sheng <huafeng.sheng@cloud-iq.com>
2019-02-22 18:08:11 +0000

    argparse more example added

--- run_argparse.sh
+++ run_argparse.sh
@@ -6,4 +6,5 @@ python argparse_test.py 4\
                         2\
                         --minus 1\
                         -v\
-                        --list-haha 3.1415 7.22
+                        --list-haha 3.1415 7.22 \
+			--inspect-input
